07d19a4181193b6f64cc67f801727ce2399d25e5
Cuongquaidan <121221562+Cuongquaidan@users.noreply.github.com>
2024-09-06 10:53:56 +0700

    react native week 3 part1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,84 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React from "react";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const index = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: "#00CCF9",
+                justifyContent: "space-around",
+                alignItems: "center",
+                paddingVertical: 60,
+            }}
+        >
+            <View
+                style={{
+                    borderWidth: 15,
+                    borderRadius: 99,
+                    borderStyle: "solid",
+                    borderColor: "#000",
+                    width: 160,
+                    height: 160,
+                }}
+            ></View>
+            <View>
+                <Text
+                    style={{
+                        width: 189,
+                        fontWeight: "700",
+                        fontSize: 25,
+                        textAlign: "center",
+                    }}
+                >
+                    GROW {"\n"}YOUR BUSINESS
+                </Text>
+            </View>
+            <View>
+                <Text
+                    style={{
+                        width: 300,
+                        fontWeight: "700",
+                        fontSize: 15,
+                        textAlign: "center",
+                    }}
+                >
+                    We will help you to grow your business using online server
+                </Text>
+            </View>
+            <View
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: 50,
+                }}
+            >
+                <Pressable>
+                    <View style={styles.buttonYellow}>
+                        <Text style={styles.textButtonYellow}>LOGIN</Text>
+                    </View>
+                </Pressable>
+                <Pressable>
+                    <View style={styles.buttonYellow}>
+                        <Text style={styles.textButtonYellow}>SIGN UP</Text>
+                    </View>
+                </Pressable>
+            </View>
+        </View>
+    );
+};
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+export default index;
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    buttonYellow: {
+        borderRadius: 8,
+        paddingHorizontal: 30,
+        paddingVertical: 12,
+        backgroundColor: "#E3C000",
+    },
+    textButtonYellow: {
+        fontSize: 20,
+        fontWeight: 700,
+    },
 });
